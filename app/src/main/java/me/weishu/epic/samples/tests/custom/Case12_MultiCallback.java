cef74f5bd63444792c0cdbc3d90156308085886f
维术 <weishu.tws@alipay.com>
2017-12-06 11:56:11 +0800

    [TestCase]: The multi-callback case is Defective, Fix it.

--- app/src/main/java/me/weishu/epic/samples/tests/custom/Case12_MultiCallback.java
+++ app/src/main/java/me/weishu/epic/samples/tests/custom/Case12_MultiCallback.java
@@ -8,66 +8,75 @@ import com.taobao.android.dexposed.XC_MethodHook;
 /**
  * Created by weishu on 17/11/21.
  */
-
 public class Case12_MultiCallback implements Case {
     private static final String TAG = "Case12_MultiCallback";
 
     int beforeCount = 0;
     int afterCount = 0;
+
+    XC_MethodHook callback1 = new XC_MethodHook() {
+        @Override
+        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+            super.beforeHookedMethod(param);
+            Log.i(TAG, "beforeHookMethod 1");
+            beforeCount++;
+        }
+
+        @Override
+        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+            super.afterHookedMethod(param);
+            Log.i(TAG, "afterHookMethod 1");
+            afterCount++;
+        }
+    };
+
+    XC_MethodHook callback2 = new XC_MethodHook() {
+        @Override
+        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+            super.beforeHookedMethod(param);
+            Log.i(TAG, "beforeHookMethod 2 lalala ");
+            beforeCount++;
+        }
+
+        @Override
+        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+            super.afterHookedMethod(param);
+            Log.i(TAG, "afterHookMethod 2 lalala");
+            afterCount++;
+        }
+    };
+
+    XC_MethodHook callback3 = new XC_MethodHook() {
+        @Override
+        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+            super.beforeHookedMethod(param);
+            Log.i(TAG, "beforeHookMethod 3 zezeze");
+            beforeCount++;
+        }
+
+        @Override
+        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+            super.afterHookedMethod(param);
+            Log.i(TAG, "afterHookMethod 3 zezeze");
+            afterCount++;
+        }
+    };
+
     @Override
     public void hook() {
-        DexposedBridge.findAndHookMethod(Target.class, "test1", Object.class, int.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                super.beforeHookedMethod(param);
-                Log.i(TAG, "beforeHookMethod 1");
-                beforeCount++;
-            }
-
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                super.afterHookedMethod(param);
-                Log.i(TAG, "afterHookMethod 1");
-                afterCount++;
-            }
-        });
-
-        DexposedBridge.findAndHookMethod(Target.class, "test1", Object.class, int.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                super.beforeHookedMethod(param);
-                Log.i(TAG, "beforeHookMethod 2 lalala ");
-                beforeCount++;
-            }
-
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                super.afterHookedMethod(param);
-                Log.i(TAG, "afterHookMethod 2 lalala");
-                afterCount++;
-            }
-        });
-
-        DexposedBridge.findAndHookMethod(Target.class, "test1", Object.class, int.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                super.beforeHookedMethod(param);
-                Log.i(TAG, "beforeHookMethod 3 zezeze");
-                beforeCount++;
-            }
-
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                super.afterHookedMethod(param);
-                Log.i(TAG, "afterHookMethod 3 zezeze");
-                afterCount++;
-            }
-        });
+        DexposedBridge.findAndHookMethod(Target.class, "test1", Object.class, int.class, callback1);
+        DexposedBridge.findAndHookMethod(Target.class, "test1", Object.class, int.class, callback2);
+        DexposedBridge.findAndHookMethod(Target.class, "test1", Object.class, int.class, callback3);
     }
 
     @Override
     public boolean validate(Object... args) {
         new Target().test1("123", 1);
-        return beforeCount == 3 && afterCount == 3;
+        boolean ret = beforeCount == 3 && afterCount == 3;
+        // reset.
+        beforeCount = 0;
+        afterCount = 0;
+
+        return ret;
     }
 }
